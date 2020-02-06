d1211603d2542963a20e8cd96a4dd1873dfc3202
unknown <kaiser0kamal@gmail.com>
2020-02-05 21:44:33 -0500

    added css and created the  method

--- src/App.js
+++ src/App.js
@@ -8,12 +8,32 @@ class Counter extends Component {
 
    }
  }
+// this where methods live
 
+increment = () =>{
+  this.setState({
+    count: this.state.count + 1,
+  })
+}
+
+decrement = () =>{
+  this.setState({
+    count: this.state.count - 1,
+  })
+}
 
   render(){
     return (
-      <div>
+      <div className="container">
+<div className="navbar"><h1>Counter</h1></div>
+<div className="counter">
+<h1>{this.state.count} </h1>
+<button type="button" onClick={this.increment}>Increment</button>
+<button type="button"onClick={this.decrement}>Decrement</button>
+</div>
       </div>
+
+
     )
   }
 };
